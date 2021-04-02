e2c900ff1cb00db9a59f6e73e1be2149937a3097
Wilson Ramstead <ramsteadwilson@gmail.com>
2021-04-01 18:57:40 -0700

    ViewImageScreen page created

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, Alert, Button } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen/>;
 }
