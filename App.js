f87f7cdd5ede61f0751a7ac447e1f470795238f3
Wilson Ramstead <ramsteadwilson@gmail.com>
2021-04-04 13:19:17 -0700

    listings screen added

--- App.js
+++ App.js
@@ -13,8 +13,9 @@ import Screen from './app/components/Screen';
 import Icon from './app/components/Icon';
 import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
- return <AccountScreen />
+ return <ListingsScreen />
 
 }
