909907a1cbe8751121bc5c171c8687245c437e6a
Wilson Ramstead <ramsteadwilson@gmail.com>
2021-04-04 21:26:53 -0700

    added textinputs, default styles, category picker, modal

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, Alert, Button, View } from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ListingDetailScreen from './app/screens/ListingDetailScreen';
+import React, { useState } from 'react';
+import { Switch, Text, TextInput } from 'react-native';
+import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
 
-import AppText from './app/components/AppText';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import MessagesScreen from './app/screens/MessagesScreen';
 import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+]
 
 export default function App() {
- return <ListingsScreen />
+
+  const [category, setCategory] =  useState();
+
+  return (
+    <Screen>
+      <AppPicker 
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories} icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 
 }
---
+++ app/components/AppPicker.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Button, View, StyleSheet, TouchableWithoutFeedback, Modal, FlatList } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import AppText from './AppText';
+import defaultStyles from '../config/styles';
+import Screen from './Screen';
+import PickerItem from './PickerItem';
+
+function AppPicker({ icon, onSelectItem, items, placeholder, selectedItem }) {
+    const [modalVisible, setModalVisible] = useState(false)
+
+    return (
+        <React.Fragment>
+            <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+                <View style={styles.container}>
+                    {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
+                    <AppText style={styles.text}>{selectedItem ? selectedItem.label : placeholder}</AppText>
+                    {icon && <MaterialCommunityIcons name="chevron-down" size={20} color={defaultStyles.colors.medium} />}
+                </View>
+            </TouchableWithoutFeedback>
+            <Modal visible={modalVisible} animationType="slide">
+                <Screen>
+                    <Button title="Close" onPress={() => setModalVisible(false)} />
+                    <FlatList 
+                        data={items}
+                        keyExtractor={item => item.value.toString()}
+                        renderItem={({ item }) => (
+                            <PickerItem 
+                                label={item.label}
+                                onPress={() => {
+                                    setModalVisible(false);
+                                    onSelectItem(item);
+                                }}
+                            />
+                            )
+                        }
+                        />
+                </Screen>
+            </Modal>
+        </React.Fragment>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: defaultStyles.colors.light,
+        borderRadius: 25,
+        flexDirection: 'row',
+        width: '100%',
+        padding: 15,
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    icon: {
+        margin: 10
+    },
+    text: {
+        flex: 1
+    }
+})
+
+export default AppPicker;
