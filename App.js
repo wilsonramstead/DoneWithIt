4e7250622e9b8e9d515d05d5cf583ae867071ef8
Wilson Ramstead <ramsteadwilson@gmail.com>
2021-07-30 17:47:16 -0700

    added imageInput component. It's touchable to open library, delete option ...

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ListItem from './app/components/lists/ListItem';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import LoginScreen from './app/screens/LoginScreen';
@@ -6,14 +6,20 @@ import Screen from './app/components/Screen';
 import RegisterScreen from './app/screens/RegisterScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+import ImageInput from './app/components/ImageInput';
+import { Button, Image } from 'react-native';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
 
-
-  // ListItem({ title, subTitle, image, IconComponent, onPress, renderRightActions })
+  const [imageUri, setImageUri] = useState();
 
   return (
-    <ListingEditScreen />
+    <Screen>
+      <ImageInput imageUri={imageUri} onChangeImage={(uri) => setImageUri(uri)} />
+    </Screen>
   );
 
 }
--- app/components/AppTextInput.js
+++ app/components/AppTextInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import defaultStyles from '../config/styles';
 
